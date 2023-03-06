8c9dd38daeb608f26fb24379778144ec226fa9e4
Clay Carden <clayton.a.carden@gmail.com>
2023-03-05 21:48:00 -0600

    Variables Exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace VariablesExercise
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿string myName = "Clay";
+int myAge = 32;
+char middleInitial = 'A';
+bool isOver18 = true;
+double currentTemp = 65.8;
+decimal currentPrice = 35.99m;
+
+Console.WriteLine($"Name: {myName}");
+
+Console.WriteLine($"Age: {myAge}");
+Console.WriteLine($"Middle Initial: {middleInitial}");
+Console.WriteLine($"Is Over 18: {isOver18}");
+Console.WriteLine($"Current Temp: {currentTemp}");
+Console.WriteLine($"Current Price: {currentPrice}");
